fa94547f79c5fd448226f893036b8d626a764f83
Justin Tulloss <justin@bigJ.(none)>
2009-03-08 04:16:58 -0700

    Created a new page and a wrapper function for a page

--- index.clj
+++ index.clj
@@ -2,14 +2,42 @@
     (:use compojure)
 )
 
+(defn page
+    ([title body] 
+        (html [:html
+            (html [:head
+                (html [:title title])
+            ]
+            (html [:body
+                (html [:div#menu "Menu"]
+                    body
+                )
+            ])
+            )
+        ])
+    )
+)
+
 (defservlet home
     (GET "/"
-        (html [:h1 {:class "title"} "Flockr"]
-            (html [:h2 "Twitter Portal"])
+        (page "Flockr"
+            (html [:h1 {:class "title"} "Flockr"]
+                (html [:h2 "Twitter Portal"]
+                    (html [:a {:href "me"} "View My Twitter"])
+                )
+            )
         )
     )
 )
 
-(run-server {:port 8080}
+(defservlet me
+    (GET "/"
+        (page "Your Flock" (html [:h1 "Welcome"]))
+    )
+)
+
+(run-server 
+    {:port 8080}
     "/*" home
+    "/me" me
 )

5840289f5587a51da13320bda49276dbe5a80eee
Justin Tulloss <jmtulloss@gmail.com>
2009-03-19 00:24:26 -0700

    Changed up the style a bit

--- public/javascripts/flockr.js
+++ public/javascripts/flockr.js
@@ -4,6 +4,7 @@ var Flockr = new Singleton({
     __init__: function(self) {
         $(document).ready(self.initialize)
     },
+
     /* Executed when the DOM is ready */
     initialize: function(self) {
         $('.feed .title').bind("click", self.toggleFeed);
